01bca9ee84d0343cc24ff8834f42f78c4efc125f
garytate123 <garytatelufc@gmail.com>
2024-12-12 16:12:35 +0000

    SASS-10713 Refactor copy text into Hint and update tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.2.0"
+  private val hmrcMongoPlayVersion = "2.3.0"
   private val bootstrapPlay30Version = "9.5.0"
   private val hmrcPlayFrontend = "10.13.0"
 
@@ -26,13 +26,13 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.14.2"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.18.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapPlay30Version  % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.15.4"                % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"                 % Test,
+    "org.jsoup"               %  "jsoup"                      % "1.18.3"                % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1"                 % Test,
     "org.scalamock"           %% "scalamock"                  % "5.2.0"                 % Test
   )
   def apply(): Seq[ModuleID] = compile ++ test
