e8f9adb105ea4f4d50c0d3bedcd713bed437d77b
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2025-08-22 10:38:27 +0100

    wip

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,22 +18,22 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.6.0"
-  private val bootstrapPlay30Version = "9.13.0"
-  private val hmrcPlayFrontend = "12.6.0"
+  private val hmrcMongoPlayVersion = "2.7.0"
+  private val bootstrapPlay30Version = "10.1.0"
+  private val hmrcPlayFrontend = "12.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapPlay30Version  % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.20.1"                % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1"                 % Test,
-    "org.scalamock"           %% "scalamock"                  % "7.3.2"                 % Test
+    "org.jsoup"               %  "jsoup"                      % "1.21.1"                % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.2"                 % Test,
+    "org.scalamock"           %% "scalamock"                  % "7.4.1"                 % Test
   )
   def apply(): Seq[ModuleID] = compile ++ test
 
