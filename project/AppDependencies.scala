8dccba45932cd19cde2e1f47436630c4291c99c0
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-02-06 09:22:18 +0000

    MIPR-2374: dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,22 +18,22 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.11.0"
+  private val hmrcMongoPlayVersion = "2.12.0"
   private val bootstrapPlay30Version = "10.5.0"
-  private val hmrcPlayFrontend = "12.25.0"
+  private val hmrcPlayFrontend = "12.31.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapPlay30Version  % Test,
     "org.jsoup"               %  "jsoup"                      % "1.22.1"                % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.2"                 % Test,
-    "org.scalamock"           %% "scalamock"                  % "7.5.2"                 % Test
+    "org.scalamock"           %% "scalamock"                  % "7.5.5"                 % Test
   )
   def apply(): Seq[ModuleID] = compile ++ test
 
